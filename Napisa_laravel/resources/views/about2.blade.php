217f9fd976bb773e63858de02ddaa7baf6db10b0
MarkJosephNapisa <markjosephnapisa@gmail.com>
2017-10-04 15:11:34 +0800

    Add files via upload

---
+++ Napisa_laravel/resources/views/about2.blade.php
@@ -0,0 +1,35 @@
+@extends('app')
+
+@section('content')
+<div class="container">
+	<div class="row">
+		<div class="col-md-10 col-md-offset-1">
+			<div class="panel panel-primary">
+				<div class="panel-heading">About Me</div>
+
+				<div class="panel-body">
+				<p>
+					Hello! My name is Mark Joseph Y. Napisa, 18 years old, living in Tisa, Cebu City. I am a student in University of Cebu - Main Campus studying Bachelor of Science in Information Technology(BSIT). I am now a 4th year student in BSIT and still learning more about programming. One of my favorite programming languanges is PHP. For me, PHP is easy to understand than other languanges like Java and C#. Why PHP is easy to understand, because I have used HTML when I was still in highschool. Able to create web pages using HTML is new experience to me, and I want to learn more so its also one of my reasons why I enrolled in IT. 
+				</p>
+				<p>
+					My hobbies are playing basketball, playing some computer games and watching anime. Basketball helps me in making body stay in shape. My friends and classmates introduced basketball to me. Basketball helps me in releaving stress and a way to bond with my fellow friends and classmates. My favorite NBA player is Dwayne Wade a shooting guard and was part of the Miami Heat team and now he is part of the Cleveland Cavaliers. My favorite PBA player is Junemar Fajardo, center of the San Miguel Beermen team and also was star player of the UC Webmasters. He has won a lot MVPs and a lot of championships being a member of the Beermen team. 
+				</p>
+				<p>	
+					Watching anime is also my favorite hobbies. I have a lot of fun just by watching anime. My younger sister and my cousin also watches anime since I am the one that introduced it to them and is now addicted to it. I was addicted to anime when I was a 3rd year college student. Animes that have genres like romcom, slice of life, school life and mystery are what I really like. Some of my favorite animes are Hyouka a mystery/slice of life anime, Yamada kun and the 7 witches a romcom anime, and kuroko no basket a sport anime. I can say that my day won't be complete without a dose of anime. Watching anime seems to be a part of my daily life. It's like an emerging technology which is not part of the hype cycle because it has matured. Matured means that is now part of the daily lives of people. 
+				</p>
+				<p>
+					Before I close this, I just want to share a qoute from one of my favorite characters in the anime Hyouka:
+				</p>
+				<blockquote class="blockquote-reverse">	
+					<p style="font-family:Arial;">Don't change so PEOPLE will like you. Be yourself and the RIGHT PEOPLE will love the real you.</p>
+					<footer>Oreki Houtarou</footer>
+				</blockquote>	
+				<p>
+					The quote is just awesome. That's all! Thank you for your time reading this page.
+				</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+@endsection
